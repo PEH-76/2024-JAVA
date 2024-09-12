682a1a45a4acaba1c89dea02bf56338133874d0b
PEH <duknow2376@gmail.com>
2024-09-12 17:45:12 +0900

    .

--- src/Variable03.java
+++ src/Variable03.java
@@ -3,7 +3,11 @@ public class Variable03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int r = 3;
+		final double PI = 3.14;
+		double area = r * r * PI;
 		
+		System.out.println(area);
 	}
 
 }
