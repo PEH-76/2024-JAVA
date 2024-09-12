419deeb1b987fca490f7d1ea66f1e927c45adccf
PEH <duknow2376@gmail.com>
2024-09-12 17:29:38 +0900

    4

--- src/Variable02.java
+++ src/Variable02.java
@@ -3,8 +3,19 @@ public class Variable02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		final int speed = 20;
+		int count = 40;
+		
+		System.out.println(speed + " " + count);
+		
+		count = 20;
+		// speed = 40;
+		
+		System.out.println(count);
+		System.out.println("상수 speed : " + speed);
+		
 		final double PI = 3.14;
-		System.out.println("반지름이 3인 원의 넓이를 구합니다.\n");
+		System.out.println("반지름이 3인 원의 넓이를 구합니다.");
 		System.out.print(3 * 3 * PI);
 
 		
