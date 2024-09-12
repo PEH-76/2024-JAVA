3b00ab0d079f6bb28bbe1e50bae051319041a8be
PEH <duknow2376@gmail.com>
2024-09-12 18:38:19 +0900

    네

---
+++ src/PrintFormatString.java
@@ -0,0 +1,14 @@
+
+public class PrintFormatString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String item = "라면";
+		int price = 800;
+		double weight = 0.12;
+		boolean discounted = false;
+		System.out.printf("상품: %s 가격: %d원 무게: %f 할인여부: %b", item, price, weight, discounted);
+		//문자열 s, 정수 d, 실수 f, 참거짓 b
+	}
+
+}
