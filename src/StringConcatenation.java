7f550f39b957d034450385a5907997ebcfe382c5
PEH <duknow2376@gmail.com>
2024-09-12 18:50:49 +0900

    HW

---
+++ src/StringConcatenation.java
@@ -0,0 +1,12 @@
+
+public class StringConcatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Month = 8, Day = 23, Eat = 1;
+		double Weight = 5.4;
+		
+		System.out.printf("====================\n고양이의 %d월 %d일 다이어트 일지\n====================\n식사 횟수: %d\n몸무게: %.1fkg", Month, Day, Eat, Weight);
+	}
+
+}
