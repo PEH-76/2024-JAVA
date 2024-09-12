fa089ec6154ea804e2262b15a3b957294e9febcf
PEH <duknow2376@gmail.com>
2024-09-12 18:52:29 +0900

    HW

--- src/Converter.java
+++ src/Converter.java
@@ -3,9 +3,9 @@ public class Converter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double Farh = 0.3;
-		double Celc = (Farh - 32.0) / 1.8;
-		System.out.printf("화씨 %.1f도는 섭씨로 %.1f도 입니다.", Farh, Celc);
+		double Fahr = 0.3;
+		double Cels = (Fahr - 32.0) / 1.8;
+		System.out.printf("화씨 %.1f도는 섭씨로 %.1f도 입니다.", Fahr, Cels);
 	}
 
 }
